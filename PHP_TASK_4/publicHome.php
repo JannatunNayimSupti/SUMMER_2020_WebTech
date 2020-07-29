28dab09e7daa499b3589d63e63b4c094496b28c1
JannatunNayimSupti <67923666+JannatunNayimSupti@users.noreply.github.com>
2020-07-29 21:41:56 +0600

    Add files via upload

---
+++ PHP_TASK_4/publicHome.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+</head>
+<body>
+  <table border="1px" border="1" cellpadding="5" cellspacing="0" align="center">
+  <tr>
+    <td width="800px"><h2>Xcompany</h2> 
+      <ul align="right">
+        <a href="home.php">Home</a>|
+        <a href="login.php">Login</a>|
+        <a href="registration.php">Registration</a>
+      </ul>
+    </td>
+  </tr>
+  <tr align="left" height="100px">
+    <td>Welcome to Xcompany</td>
+  </tr>
+  <tr>
+    <td align="center">copyright@2017</td>
+  </tr>
+</table>
+</body>
+</html>
