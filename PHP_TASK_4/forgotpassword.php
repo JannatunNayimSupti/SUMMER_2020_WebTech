2b008c0cbb3f8e0873e487991cce4249bc75e6da
JannatunNayimSupti <67923666+JannatunNayimSupti@users.noreply.github.com>
2020-07-29 21:44:40 +0600

    Add files via upload

---
+++ PHP_TASK_4/forgotpassword.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+</head>
+<body>
+  <table border="1px" border="1" cellpadding="5" cellspacing="0" align="center">
+  <tr>
+    <td width="800px"><h2>Xcompany</h2> 
+      <ul align="right">
+        <a href="home.php">Home</a>|
+        <a href="login.php">Login</a>|
+        <a href="registration.php">Registration</a>
+      </ul>
+    </td>
+  </tr>
+  <tr height="300px">
+    <td>
+      <form>
+        <fieldset width="500px">
+          <legend>FORGOT PASSWORD</legend>
+          <table>
+            <tr>
+              <td>Enter Email</td>
+              <td>: <input type="email" name="email"></td>
+            </tr>
+          </table>
+          <hr>
+          <input type="submit" name="submit">
+        </fieldset>
+      </form>
+    </td>
+  </tr>
+  <tr>
+    <td align="center">copyright@2017</td>
+  </tr>
+</table>
+</body>
+</html>
