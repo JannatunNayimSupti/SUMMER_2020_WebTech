ef14d92966789bb3fc32bb2e0cb6c035d5a31539
JannatunNayimSupti <67923666+JannatunNayimSupti@users.noreply.github.com>
2020-07-29 11:30:00 +0600

    Add files via upload

---
+++ PHP_TASK_4/editprofile.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+</head>
+<body>
+  <table border="1px" border="1" cellpadding="5" cellspacing="0" align="center">
+  <tr >
+    <td colspan="2" width="900px"><h2>Xcompany</h2> 
+      <ul align="right">
+        <a href="home.php">Home</a>|
+        <a href="login.php">Login</a>|
+        <a href="registration.php">Registration</a>
+      </ul>
+    </td>
+  </tr>
+  <tr height="300px">
+    <td>
+        <ul>
+            <li><a href="#"> Dashboard </a></li>
+            <li><a href="#"> View Profile </a></li>
+            <li><a href="#"> Edit Profile </a> </li>
+            <li><a href="#"> Change Profile Picture </a></li>
+            <li><a href="#"> Change Password </a></li>
+            <li><a href="#"> Logout </a> </li>
+        </ul>
+    </td>
+    <td width="60%">
+        <form>
+            <fieldset>
+    <legend><b>EDIT PROFILE</b></legend>
+    <form>
+        <br/>
+        <table width="100%" cellpadding="0" cellspacing="0">
+            <tr>
+                <td>Name</td>
+                <td>:</td>
+                <td><input name="name" type="text"></td>
+                <td></td>
+            </tr>       
+            <tr><td colspan="4"><hr/></td></tr>
+            <tr>
+                <td>Email</td>
+                <td>:</td>
+                <td>
+                    <input name="email" type="text">
+                    <abbr title="hint: sample@example.com"><b>i</b></abbr>
+                </td>
+                <td></td>
+            </tr>       
+      
+            <tr><td colspan="4"><hr/></td></tr>
+            <tr>
+                <td>Date Of Birth</td>
+                <td>:</td>
+                <td>
+                    <input name="DOB" type="Date">
+                </td>
+                <td></td>
+            </tr>
+        </table>
+        <hr/>
+        <input type="submit" value="Submit">
+        <input type="reset">
+    </form>
+</fieldset>
+        </form>
+    </td>
+    <tr>
+        <td colspan="2" align="center">copyright@2017</td>
+    </tr>
+</table>
+</body>
+</html>
